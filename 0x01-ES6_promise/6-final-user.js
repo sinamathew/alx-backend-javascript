2a3829596766b41f01d34935453d8f4bf0cd0471
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-05-30 00:40:04 +0100

    Promise is a debt

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,19 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return new Promise((resolve, reject) => {
-    signUpUser(firstName, lastName)
-      .then(() => {
-        uploadPhoto(fileName)
-          .then((result) => {
-            resolve({ status: 'fulfilled', value: result });
-          })
-          .catch((error) => {
-            resolve({ status: 'rejected', value: error.toString() });
-          });
-      })
-      .catch((error) => {
-        reject(error.toString());
-      });
-  });
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
