efb4c08ac50977403cda9c1779dacb3280834d42
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-05-29 23:17:25 +0100

    Figuring out how promise works

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,8 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => { return { status: 200, body: 'success' }; }) // eslint-disable-line arrow-body-style
-    .catch(() => { throw new Error(); })
-    .finally(() => console.log('Got a response from the API'));
+    .then(() => {
+      console.load('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => { throw new Error(); });
 }
